b7ae221b17dd2130eae83f9b4b2a7e4fa4a67cd2
Paricia Muñoz <patrimdm96@gmail.com>
2020-10-30 20:43:20 +0000

    Create basic structure and include basic information fof each document needed

--- Canvas Asteroids Game/js/obstacle.js
+++ Canvas Asteroids Game/js/obstacle.js
@@ -1,3 +1,33 @@
 class Obstacle {
-    
+    constructor(ctx, canvasWidth, canvasHeight, color) {
+        this.ctx = ctx
+        this.obsSize = {
+            w: 20,
+            h: 20
+        }
+        this.canvasSize = {
+            w: canvasWidth,
+            h: canvasHeight
+        }
+        this.obsPos = {
+            x: undefined,   //Redefinir para que salgan de todas partes
+            y: undefined,
+        }
+        this.obsVel = {
+            x: 10,
+            y: 10
+        }
+        this.color = color
+    }
+
+    drawObs() {
+        this.ctx.fillStyle = this.color
+        this.ctx.fillRect(this.obsPos.x, this.obsPos.y, this.obsSize.w, this.obsSize.h)
+        this.move()
+    }
+
+    moveObs() {
+        this.obsPos.x += this.obsVel.x
+        this.obsPos.y += this.obsVel.y
+    }
 }
--- Canvas Asteroids Game/js/player.js
+++ Canvas Asteroids Game/js/player.js
@@ -13,10 +13,32 @@ class Player {
             w: this.canvasSize.w / 2 - (this.playerSize.w / 2),
             h: this.canvasSize.h / 2 - (this.playerSize.h / 2)
         };
-        this.image = new Image();
-        this.image.src = "./img/player.png";
+        this.playerInstance = new Image();
+        this.playerInstance.src = "./img/player.png";
         this.keys = keys;
         this.bullets = [];
 
-    }    
+        this.eventListeners();
+    }
+
+    drawPlayer() {
+        this.ctx.drawImage(this.playerInstance, this.playerPos.x, this.playerPos.y, this.playerSize.w, this.playerSize.h)
+        this.bullets.forEach(b => b.draw())
+        this.clearBullets()
+    }
+
+    shoot() {
+        //constructor(ctx, playerPosX, playerPosY, playerWidth, playerHeight, color)
+        this.bullets.push(new Bullet(this.ctx, this.playerPos.x, this.playerPos.y, this.playerSize.w, this.playerSize.h, 'black'))
+    }
+
+    clearBullets() {
+        this.bullets = this.bullets.filter(//Bullets inside screen)
+    }
+
+    eventListeners() {
+        document.addEventListener("keydown", e => {
+            //INSERT KEYS AND MOVEMENTS
+        })
+    }
 }
